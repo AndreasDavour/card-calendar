478a62877115c36f5049bf64c65ec06544324d5a
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-10-01 15:40:20 +0200

    added function for sum up until specified date, and prettifying

--- calendar.lisp
+++ calendar.lisp
@@ -3,20 +3,6 @@
 
 (asdf:load-system "cl-ppcre")
 
-(defun calendar (date)
-  "Returns the card in a regular deck of cards,
-matching the number of that week of the year, 
-and the day in that week. Date expected in YYYY-MM-DD
-format."
-  (let* ((result)
-	 (split-data (cl-ppcre:split "-" date))
-	 (year (first split-data))
-	 (month (second split-data))
-	 (day (third split-data)))
-;    (multiple-value-bind (year month day) (cl-ppcre:scan date)
-					;     (setf result nil))
-    split-data))
-
 ;;;; Plan
 ;;;
 ;;; figure out the days of a month
@@ -25,24 +11,16 @@ format."
 ;;; divide by 52 and use that as an index into card deck
 ;;; figure out how many days into that card we are
 
-;;; Leap years
+;;; Leap years and infrastructure data structures
 ;;;
 
 (defun leap-yearp (year)
   "Predicate checking true for leap years"
   (cond
-    ((not (zerop (mod year 4)))
-     ; common year
-     nil)
-    ((not (zerop (mod year 100)))
-     ; leap year
-     t)
-    ((not (zerop (mod year 400)))
-     ; common year
-     nil)
-    (t
-     ; leap year
-     t)))
+    ((not (zerop (mod (parse-integer year) 4)))     nil)
+    ((not (zerop (mod (parse-integer year) 100)))   t)
+    ((not (zerop (mod (parse-integer year) 400)))   nil)
+    (t                                              t)))
 
 ;;; days in a month
 (defun days-in-february (year)
@@ -65,14 +43,45 @@ format."
 
 (defparameter months '("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December"))
 
+
+;;; worker functions
+
 (defun sum-days-to-month (month yr)
 "Sums the number of days until MONTH, exclusive"
-  (let ((sum 0))
+  (let ((sum 0)
+	(month-name (nth (1- month) months)))
+    ;; sometimes I wonder if LOOP is butt-ugly, or just neat...
     (loop :for m :in months
-	  :until (string= (first (assoc m number-of-days :test #'string=)) month)
+	  :until (string= (first (assoc m number-of-days :test #'string=)) month-name)
+	  ;; just sum the days of each month, except February which need extra tests
 	  :if (string= m "February")
 	    :do (setf sum (+ sum (days-in-february yr)))
 	  :else
 	    :do (setf sum (+ sum (rest (assoc m number-of-days :test #'string=))))
 	  :end)
     sum))
+
+(defun sum-days-until-now (d m y)
+  "Sums the Days until this Month (exclusive), 
+adding the Days of the present Month, adding 
+leap day if needed."
+  ;; hiding the string->integer conversion here
+  ;; in order to make top function a bit prettier
+  (+ (parse-integer d) (sum-days-to-month (parse-integer m) y)))
+
+
+;;; top function
+(defun calendar (date)
+  "Returns the card in a regular deck of cards,
+matching the number of that week of the year, 
+and the day in that week. Date expected in YYYY-MM-DD
+format."
+  (let* ((result)
+	 (split-data (cl-ppcre:split "-" date))
+	 (year (first split-data))
+	 (month (second split-data))
+	 (day (third split-data)))
+;    (multiple-value-bind (year month day) (cl-ppcre:scan date)
+					;     (setf result nil))
+    (sum-days-until-now day month year)))
+
