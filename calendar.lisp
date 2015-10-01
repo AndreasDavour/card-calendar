5da632c699de8415b6240bfec51679144602bcb6
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-10-01 13:20:35 +0200

    added functionality to sum the days up until a specified month

--- calendar.lisp
+++ calendar.lisp
@@ -1,11 +1,21 @@
 ;;;; A calendar based on the fact there are 52 cards in a deck, and 52
 ;;;; weeks in a year
 
+(asdf:load-system "cl-ppcre")
+
 (defun calendar (date)
-  (let ((result))
-    (multiple-value-bind (year month day) (cl-ppcre:scan date)
-      (setf result nil))
-    result))
+  "Returns the card in a regular deck of cards,
+matching the number of that week of the year, 
+and the day in that week. Date expected in YYYY-MM-DD
+format."
+  (let* ((result)
+	 (split-data (cl-ppcre:split "-" date))
+	 (year (first split-data))
+	 (month (second split-data))
+	 (day (third split-data)))
+;    (multiple-value-bind (year month day) (cl-ppcre:scan date)
+					;     (setf result nil))
+    split-data))
 
 ;;;; Plan
 ;;;
@@ -34,3 +44,35 @@
      ; leap year
      t)))
 
+;;; days in a month
+(defun days-in-february (year)
+  (if (leap-yearp year)
+      29
+      28))
+
+(defparameter number-of-days '(("January" . 31)
+			       ("February" . 0)
+			       ("March" . 31)
+			       ("April" . 30)
+			       ("May" . 31)
+			       ("June" . 30)
+			       ("July". 31)
+			       ("August" . 31)
+			       ("September" . 30)
+			       ("October" . 31)
+			       ("November" . 30)
+			       ("December" . 31)))
+
+(defparameter months '("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December"))
+
+(defun sum-days-to-month (month yr)
+"Sums the number of days until MONTH, exclusive"
+  (let ((sum 0))
+    (loop :for m :in months
+	  :until (string= (first (assoc m number-of-days :test #'string=)) month)
+	  :if (string= m "February")
+	    :do (setf sum (+ sum (days-in-february yr)))
+	  :else
+	    :do (setf sum (+ sum (rest (assoc m number-of-days :test #'string=))))
+	  :end)
+    sum))
